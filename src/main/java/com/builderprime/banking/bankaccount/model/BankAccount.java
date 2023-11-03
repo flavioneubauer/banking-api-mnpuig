d8f1df4ecaef1da25960adb48a1a1674983f36e1
Flavio Neubauer <flavio.neubauer@gmail.com>
2023-11-03 10:48:50 -0300

    fix unused imports

--- src/main/java/com/builderprime/banking/bankaccount/model/BankAccount.java
+++ src/main/java/com/builderprime/banking/bankaccount/model/BankAccount.java
@@ -1,28 +1,12 @@
 package com.builderprime.banking.bankaccount.model;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.UUID;
-
 import com.builderprime.banking.customer.model.Customer;
-import com.builderprime.banking.transfer.model.Transfer;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Getter
 @Setter
